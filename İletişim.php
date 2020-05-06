886c62816b439a88292ff107437cd6af4d02afae
bgrdmrl <rdemirel2001@gmail.com>
2020-05-06 18:48:29 +0300

    İletişim.html sayfam bu sayfaya host ediyor

---
+++ İletişim.php
@@ -0,0 +1,34 @@
+<table>
+    <tr>
+        <td>Adınız = </td>
+        <td><?php echo $_POST['ad'] ?></td>
+    </tr>
+    <tr>
+        <td>Soyadınız = </td>
+        <td><?php echo $_POST['soyad'] ?></td>
+    </tr>
+    <tr>
+        <td>e-mail Adresiniz = </td>
+        <td><?php echo $_POST['email'] ?></td>
+    </tr>
+    <tr>
+        <td>Şifreniz = </td>
+        <td><?php echo $_POST['sifre'] ?></td>
+    </tr>
+    <tr>
+        <td>Cinsiyetiniz = </td>
+        <td><?php echo $_POST['cinsiyet'] ?></td>
+    </tr>
+    <tr>
+        <td>Doğum Tarihiniz = </td>
+        <td><?php echo $_POST['dogumTarihi'] ?></td>
+    </tr>
+    <tr>
+        <td>Yaşınız = </td>
+        <td><?php echo $_POST['yas'] ?></td>
+    </tr>
+    <tr>
+        <td>Mesajınız = </td>
+        <td><?php echo $_POST['mesaj'] ?></td>
+    </tr>
+</table>
